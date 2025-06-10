6f66bae4bb1dfc90fe139179bfcad607990df925
YuliaRey12 <ostrovskaya118@yandex.ru>
2025-06-10 18:56:14 +0700

    Получение ссылок на просмотр элементов

--- app/src/main/java/com/example/lab3shihova/MainActivity.kt
+++ app/src/main/java/com/example/lab3shihova/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.lab3shihova
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val inputText = findViewById<EditText>(R.id.inputText)
+        val reverseButton = findViewById<Button>(R.id.reverseButton)
+        val resultText = findViewById<TextView>(R.id.resultText)
+
     }
 }
